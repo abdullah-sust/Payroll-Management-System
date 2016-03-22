2d8458187dc2cab8d2ddf76eb7be2eb9da328ace
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2016-03-22 18:35:28 +0600

    Conflict solved

--- app/models/CompanyProfile.php
+++ app/models/CompanyProfile.php
@@ -2,9 +2,6 @@
 
 class CompanyProfile extends \Eloquent {
 	protected $fillable = [];
-<<<<<<< HEAD
-	protected $table = "company_profile";
-=======
 	protected $table = 'company_profile';
->>>>>>> refs/remotes/origin/abdullah
+
 }
