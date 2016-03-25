83a411d6506cac8d9928c95909a629191c72a723
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2016-03-25 10:07:54 +0600

    Pushed to master

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -6,4 +6,21 @@ class Profile extends \Eloquent {
 	public function user() {
 		return $this->belongsTo('User', 'user_id', 'id');
 	}
+
+	public static function random() {
+		$value =  mt_rand(100100,109999);
+		return $value;
+	}
+
+	public static function rand_uniq() {
+		$value = Profile::random();
+		if(Profile::randExist($value)) {
+			return rand_uniq();
+		}
+		return $value;
+	}
+
+	public static function randExist($value){
+		return User::where('employeeID', $value)->exists();
+	}
 }
